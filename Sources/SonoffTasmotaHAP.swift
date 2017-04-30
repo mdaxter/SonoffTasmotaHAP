b33e47823640b69e7ec8c89fc40b4f1b4c53b14b
Mike <mdaxter@users.noreply.github.com>
2017-05-01 06:53:29 +1000

    Initial import

---
+++ Sources/SonoffTasmotaHAP.swift
@@ -0,0 +1,4 @@
+struct SonoffTasmotaHAP {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import SonoffTasmotaHAPTests
+
+XCTMain([
+    testCase(SonoffTasmotaHAPTests.allTests),
+])
