4de20651f321231626bb4aae0b73e42dc18f93f2
Mike <mdaxter@users.noreply.github.com>
2017-05-01 07:01:15 +1000

    Split into library and executable

--- Package.swift
+++ Package.swift
@@ -3,5 +3,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SonoffTasmotaHAP"
+    name: "SonoffTasmotaHAP",
+    targets: [
+        Target(name: "SonoffTasmotaHAP", dependencies: []),
+        Target(name: "sonoff-tasmota-hap-bridge", dependencies: ["SonoffTasmotaHAP"]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/Bouke/HAP.git", majorVersion: 0),
+    ]
 )
